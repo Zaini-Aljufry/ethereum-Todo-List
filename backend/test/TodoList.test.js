a26f7400aa5f3cb3fee784da63d2c3606ba0de24
Zaini-Aljury <Zaini.Al-Jufry@sg.ey.com>
2020-07-18 02:20:18 +0800

    completed smart contract to be use for frontend"

--- backend/test/TodoList.test.js
+++ backend/test/TodoList.test.js
@@ -19,4 +19,24 @@ contract('TodoList',(accounts)=>{
 
     })
 
+    it('should be able to create task', async() =>{
+        const result = await this.todoList.createTask('And connect it to react')
+        const taskCount = await this.todoList.taskCount()
+        assert.equal(taskCount,2)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(),2)
+        assert.equal(event.content,'And connect it to react')
+        assert.equal(event.completed,false)
+    })
+
+    it('should be able to toggle completed',async () =>{
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal( task.completed,true)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(),1)
+        assert.equal(event.completed,true)
+
+    })
+
 })
